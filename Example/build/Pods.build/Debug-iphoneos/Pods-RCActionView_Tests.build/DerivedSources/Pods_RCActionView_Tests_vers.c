55765be7abd235cfabbd18e9d693c16a22143439
Rodrigo <flipacholas@gmail.com>
2016-06-18 18:16:59 +0100

    Release 1.0.0

--- Example/build/Pods.build/Debug-iphoneos/Pods-RCActionView_Tests.build/DerivedSources/Pods_RCActionView_Tests_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char Pods_RCActionView_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_RCActionView_Tests  PROJECT:Pods-1" "\n"; const double Pods_RCActionView_TestsVersionNumber __attribute__ ((used)) = (double)1.;
--- Example/build/Pods.build/Debug-iphoneos/RCActionView.build/DerivedSources/RCActionView_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char RCActionViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RCActionView  PROJECT:Pods-1" "\n"; const double RCActionViewVersionNumber __attribute__ ((used)) = (double)1.;
